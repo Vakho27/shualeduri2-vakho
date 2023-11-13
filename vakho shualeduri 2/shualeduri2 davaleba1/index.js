cd2e3f07a30c33ccbec6e7388b4522b2c313d5bd
Vakho27 <128822680+Vakho27@users.noreply.github.com>
2023-11-13 18:28:25 +0400

    Add files via upload

---
+++ vakho shualeduri 2/shualeduri2 davaleba1/index.js
@@ -0,0 +1,44 @@
+//davaleba1
+
+// let age = 25; 
+
+// if (age >= 18) {
+//     console.log("მომხმარებლის ასაკი არის 18 ან ზემოთ");
+// } else {
+//     console.log("მომხმარებლის ასაკი არის 18-ზე ქვემოთ");
+// }
+
+
+
+//davaleba 2
+
+
+let weekDay = 8;
+
+switch (weekDay) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Your week day is not correct");
+}
+
+
